38dd806d6011a213b60398ecf39148af6d0940ec
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-03-17 20:32:03 -0300

    Update CursosController.js

--- public/js/controllers/CursosController.js
+++ public/js/controllers/CursosController.js
@@ -1,16 +1,33 @@
 angular.module('ifsp').controller('CursosController',
-	function($scope) {
-		$scope.total = 0;
-		$scope.incrementa = function() {
-			$scope.total++;
-		};
-
-		$scope.cursos  = [
-		{_id: 1, curso: 'Engenharia de Produção', coordenador: 'fabio.teixeira@ifsp.edu.br'},
- 		{_id: 2, curso: 'Tecnologia em Análise e Desenvolvimento de Sistemas', coordenador: 'fabiano.teixeira@ifsp.edu.br'},
- 		{_id: 3, curso: 'Licenciatura em Letras Português/Inglês', coordenador: 'melissa.teixeira@ifsp.edu.br'},
- 		{_id: 4, curso: 'Tecnologia em Gestão Pública', coordenador: 'melissa.teixeira@ifsp.edu.br'}
-        ];
-
+    function($resource, $scope, $http) {
+        $scope.cursos = [];
         $scope.filtro = '';
-});
+        var Cursos = $resource('/cursos');
+		var contatos = $http.get('/cursos');
+		var promise = $http.get('/cursos');
+        function buscaCursos() {
+            Cursos.query(
+                function(cursos) {
+                    $scope.cursos = cursos;
+                },
+                function(erro) {
+                    console.log('Não foi possível obter a lista de cursos.');
+                    console.log(erro);
+                }
+            );
+        }
+
+        promise
+        .then(exibeCursos)
+        .then(modificaCursos)
+        .then(AtualizaCursos)
+        .then(function(cursos) {
+             $scope.mensagem = {texto: 'Cursos atualizados com sucesso'};
+        })
+        .catch(function(erro) {
+        console.log(erro.status)
+        console.log(erro.statusText)
+        });
+
+        buscaCursos();
+    });
