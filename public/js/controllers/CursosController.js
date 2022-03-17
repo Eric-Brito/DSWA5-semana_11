77072ce6cd9c565c4e67cbcb4c1755f540799465
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-03-17 20:53:26 -0300

    Update CursosController.js

--- public/js/controllers/CursosController.js
+++ public/js/controllers/CursosController.js
@@ -2,7 +2,7 @@ angular.module('ifsp').controller('CursosController',
     function($resource, $scope, $http) {
         $scope.cursos = [
             {"_id": 1, "curso": "Engenharia da Produção", "coordenador": "fabio.teixeira@ifsp.edu.br"},
-	        {"_id": 2, "curso": "Tecnologia em Análise e Desenvolvimento de Sistemas","coordenador": "fabiano.teixeira@ifsp.edu.br"},
+	    {"_id": 2, "curso": "Tecnologia em Análise e Desenvolvimento de Sistemas","coordenador": "fabiano.teixeira@ifsp.edu.br"},
             {"_id": 3, "curso": "Licenciatura em Letras Português/Inglês","coordenador": "melissa.teixeira@ifsp.edu.br"},
             {"_id": 3, "curso": "Tecnologia em Gestão Pública","coordenador": "melissa.teixeira@ifsp.edu.br"}           
         ];
