f382985fdde81d73019c09cca2c6563b1ae4423f
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-03-17 20:53:48 -0300

    Update ContatosController.js

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,6 +1,10 @@
 angular.module('ifsp').controller('ContatosController',
 	function($resource, $scope, $http) {
-		$scope.contatos = [];
+		$scope.contatos = [
+			{"_id": 1, "nome": "Fabio Teixeira", "email": "fabio.teixeira@ifsp.edu.br"},
+	                {"_id": 2, "nome": "Fabiano Teixeira","email": "fabiano.teixeira@ifsp.edu.br"},
+                        {"_id": 3, "nome": "Melissa Teixeira","email": "melissa.teixeira@ifsp.edu.br"}
+		];
 		$scope.filtro = '';
 		var Contato = $resource('/contatos');
 		var contatos = $http.get('/contatos');
