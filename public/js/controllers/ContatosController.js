27665b0df99c27270e796111636fe7f0dd7daab0
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-03-17 20:30:11 -0300

    Update ContatosController.js

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,19 +1,33 @@
 angular.module('ifsp').controller('ContatosController',
-    function($resource, $scope) {
-        $scope.contatos = [];
-        $scope.filtro = '';
-        var Contato = $resource('/contatos');
+	function($resource, $scope, $http) {
+		$scope.contatos = [];
+		$scope.filtro = '';
+		var Contato = $resource('/contatos');
+		var contatos = $http.get('/contatos');
+		var promise = $http.get('/contatos');
+		function buscaContatos() {
+			Contato.query(
+				function(contatos) {
+					$scope.contatos = contatos;
+				},
+				function(erro) {
+					console.log("Não foi possível obter a lista de contatos");
+					console.log(erro);
+				}
+			);
+		}
 
-        function buscaContatos() {
-            Contato.query(
-                function(contatos) {
-                    $scope.contatos = contatos;
-                },
-                function(erro) {
-                    console.log("Não foi possível obter a lista de contatos");
-                    console.log(erro);
-                }
-            );
-        }
-        buscaContatos()
-    });
+		promise
+		       .then(exibeContatos)
+			   .then(modificaContatos)
+			   .then(AtualizaContatos)
+			   .then(function(contatos) {
+				    $scope.mensagem = {texto: 'Contatos atualizados com sucesso'};
+			   })
+			   .catch(function(erro) {
+               console.log(erro.status);
+			   console.log(erro.statusText);
+			   });
+
+		buscaContatos();
+	});
