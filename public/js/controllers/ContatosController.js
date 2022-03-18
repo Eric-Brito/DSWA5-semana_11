cab541169ae93471e5030cc396ad26fd8b175671
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-03-17 21:18:30 -0300

    Update ContatosController.js

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,37 +1,19 @@
 angular.module('ifsp').controller('ContatosController',
-	function($resource, $scope, $http) {
-		$scope.contatos = [
-			{"_id": 1, "nome": "Fabio Teixeira", "email": "fabio.teixeira@ifsp.edu.br"},
-	                {"_id": 2, "nome": "Fabiano Teixeira","email": "fabiano.teixeira@ifsp.edu.br"},
-                        {"_id": 3, "nome": "Melissa Teixeira","email": "melissa.teixeira@ifsp.edu.br"}
-		];
-		$scope.filtro = '';
-		var Contato = $resource('/contatos');
-		var contatos = $http.get('/contatos');
-		var promise = $http.get('/contatos');
-		function buscaContatos() {
-			Contato.query(
-				function(contatos) {
-					$scope.contatos = contatos;
-				},
-				function(erro) {
-					console.log("Não foi possível obter a lista de contatos");
-					console.log(erro);
-				}
-			);
-		}
+    function($resource, $scope) {
+        $scope.contatos = [];
+        $scope.filtro = '';
+        var Contato = $resource('/contatos');
 
-		promise
-		        .then(exibeContatos)
-			 .then(modificaContatos)
-			 .then(AtualizaContatos)
-			 .then(function(contatos) {
-				  $scope.mensagem = {texto: 'Contatos atualizados com sucesso'};
-			 })
-			 .catch(function(erro) {
-                         console.log(erro.status);
-			 console.log(erro.statusText);
-			 });
-
-		buscaContatos();
-	});
+        function buscaContatos() {
+            Contato.query(
+                function(contatos) {
+                    $scope.contatos = contatos;
+                },
+                function(erro) {
+                    console.log("Não foi possível obter a lista de contatos");
+                    console.log(erro);
+                }
+            );
+        }
+        buscaContatos()
+    });
