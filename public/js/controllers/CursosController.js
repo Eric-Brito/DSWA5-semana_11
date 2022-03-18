bf8a8788e505c7d127b01a15ba1e39d10ced3672
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-03-17 21:23:19 -0300

    Update CursosController.js

--- public/js/controllers/CursosController.js
+++ public/js/controllers/CursosController.js
@@ -4,12 +4,7 @@
 			$scope.total++;
 		};
 
-		$scope.cursos  = [
-		{_id: 1, curso: 'Engenharia de Produção', coordenador: 'fabio.teixeira@ifsp.edu.br'},
- 		{_id: 2, curso: 'Tecnologia em Análise e Desenvolvimento de Sistemas', coordenador: 'fabiano.teixeira@ifsp.edu.br'},
- 		{_id: 3, curso: 'Licenciatura em Letras Português/Inglês', coordenador: 'melissa.teixeira@ifsp.edu.br'},
- 		{_id: 4, curso: 'Tecnologia em Gestão Pública', coordenador: 'melissa.teixeira@ifsp.edu.br'}
-        ];
+		$scope.cursos  = [ ];
 
         $scope.filtro = '';
 });
